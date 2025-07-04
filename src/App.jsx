53f0c64efee038923efb4f4e6bd7441c608b9cf5
MJ1305 <folahunazeez@gmail.com>
2025-07-04 15:04:16 +0100

    redesigned loginCard to fit ATM structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import LoginCard from "./components/Loginpage";
+
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center p-4 bg-gradient-to-br from-gray-900 to-blue-900">
